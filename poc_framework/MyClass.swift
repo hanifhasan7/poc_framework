22e885c1acb7d14a1d9de40bd2d1d199538aa199
hanifhasan <hanif@nssit.com.my>
2024-07-10 11:45:24 +0800

    fist commit

---
+++ poc_framework/MyClass.swift
@@ -0,0 +1,17 @@
+//
+//  MyClass.swift
+//  poc_framework
+//
+//  Created by user on 10/07/2024.
+//
+
+import Foundation
+
+
+public class MyClass {
+    public init() {}
+
+    public func sayHello() {
+        print("Hello from MyFramework!")
+    }
+}
---
+++ poc_framework/poc_framework.h
@@ -0,0 +1,18 @@
+//
+//  poc_framework.h
+//  poc_framework
+//
+//  Created by user on 10/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for poc_framework.
+FOUNDATION_EXPORT double poc_frameworkVersionNumber;
+
+//! Project version string for poc_framework.
+FOUNDATION_EXPORT const unsigned char poc_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <poc_framework/PublicHeader.h>
+
+
